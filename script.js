de49077da1c4e72f31b8494e10480092e6bbc460
Alex Bruno <alex.bruno102004@gmail.com>
2021-09-03 22:01:09 -0300

    JavaScript add to the project

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-const button = document.getElementById('converter')
+const button = document.getElementById('converter-button')
+const select = document.getElementById('select-input')
 
-console.log (button)
 
-const dolar = 5.2
+const dolar = 6.2
 
 const convertValues = () => {
 
-    const inputReais = document.getElementById ('real').value
-    console.log (inputReais / dolar)
-   
+    const inputReais = document.getElementById('input-real').value
+
+    const realValueText = document.getElementById('real-value-text')
+
+    realValueText.innerHTML =   new Intl.NumberFormat('pt-BR',
+    { style: 'currency', currency: 'BRL' }
+  ).format((inputReais));
+
+    const currencyDolar = document.getElementById('currency-dolar')
+
+    currencyDolar.innerHTML = new Intl.NumberFormat('en-US',
+    { style: 'currency', currency: 'USD' }
+  ).format((inputReais / dolar));
+
+
+
+  
+
+    console.log(inputReais / dolar)
+
+
 }
 
 
-button.addEventListener ('click', convertValues)
-
-
+button.addEventListener('click', convertValues)
+select.addEventListener('change', convertValues)
 
 
 
